d6ddea9ee616b546f0d6e4954f24699426088702
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-10-20 15:13:39 +0200

    structure changes

---
+++ static/scripts/datacleanup.js
@@ -0,0 +1,274 @@
+let colors = [
+    {
+      "Lievelingskleur": "#673dff"
+    },
+    {
+      "Lievelingskleur": "#22376b"
+    },
+    {
+      "Lievelingskleur": "#57E5C3"
+    },
+    {
+      "Lievelingskleur": "#212738"
+    },
+    {
+      "Lievelingskleur": "000000"
+    },
+    {
+      "Lievelingskleur": "103FF0"
+    },
+    {
+      "Lievelingskleur": "#00DCB9"
+    },
+    {
+      "Lievelingskleur": "232323"
+    },
+    {
+      "Lievelingskleur": "#7bb7cf"
+    },
+    {
+      "Lievelingskleur": "FFFFFF"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "Legergroen"
+    },
+    {
+      "Lievelingskleur": "#4a2040"
+    },
+    {
+      "Lievelingskleur": "#2F909E"
+    },
+    {
+      "Lievelingskleur": "#008FFF"
+    },
+    {
+      "Lievelingskleur": "#ff7b00"
+    },
+    {
+      "Lievelingskleur": "#1b3e84"
+    },
+    {
+      "Lievelingskleur": "#00800"
+    },
+    {
+      "Lievelingskleur": "0000FF"
+    },
+    {
+      "Lievelingskleur": "#ff0000"
+    },
+    {
+      "Lievelingskleur": "#fcba03"
+    },
+    {
+      "Lievelingskleur": "#e6b5ff"
+    },
+    {
+      "Lievelingskleur": "#ff0000"
+    },
+    {
+      "Lievelingskleur": "d05339"
+    },
+    {
+      "Lievelingskleur": "#40E0D0"
+    },
+    {
+      "Lievelingskleur": "1d79f6"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "Staal."
+    },
+    {
+      "Lievelingskleur": "#66169e"
+    },
+    {
+      "Lievelingskleur": "#00FFF7"
+    },
+    {
+      "Lievelingskleur": "#354e71"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#5EEAC2"
+    },
+    {
+      "Lievelingskleur": "#3238a8"
+    },
+    {
+      "Lievelingskleur": "#97ffbb; #e77a8a"
+    },
+    {
+      "Lievelingskleur": "#090E94"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "245c24"
+    },
+    {
+      "Lievelingskleur": "/"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#FF5C00"
+    },
+    {
+      "Lievelingskleur": "2D3748"
+    },
+    {
+      "Lievelingskleur": "#FFCC00"
+    },
+    {
+      "Lievelingskleur": "#3a7bcf"
+    },
+    {
+      "Lievelingskleur": "#1E41F6"
+    },
+    {
+      "Lievelingskleur": "#ffffff"
+    },
+    {
+      "Lievelingskleur": "#336255"
+    },
+    {
+      "Lievelingskleur": "#00db73"
+    },
+    {
+      "Lievelingskleur": "#52307c"
+    },
+    {
+      "Lievelingskleur": "#0469c7"
+    },
+    {
+      "Lievelingskleur": "grijs"
+    },
+    {
+      "Lievelingskleur": "#2a9d8f"
+    },
+    {
+      "Lievelingskleur": "#FFE800"
+    },
+    {
+      "Lievelingskleur": "#0DA180"
+    },
+    {
+      "Lievelingskleur": "#f49235"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#f9c7e3"
+    },
+    {
+      "Lievelingskleur": "#f2cb0c"
+    },
+    {
+      "Lievelingskleur": "#6a1713"
+    },
+    {
+      "Lievelingskleur": "#067881"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#d2a8ff"
+    },
+    {
+      "Lievelingskleur": "1976D2"
+    },
+    {
+      "Lievelingskleur": "87CEFA"
+    },
+    {
+      "Lievelingskleur": "#6C172D"
+    },
+    {
+      "Lievelingskleur": "#6C172D"
+    },
+    {
+      "Lievelingskleur": "Zwart"
+    },
+    {
+      "Lievelingskleur": "#35682d"
+    },
+    {
+      "Lievelingskleur": "#33ccff"
+    },
+    {
+      "Lievelingskleur": "696969"
+    },
+    {
+      "Lievelingskleur": "rood"
+    },
+    {
+      "Lievelingskleur": "#97DDC1"
+    },
+    {
+      "Lievelingskleur": "a85418"
+    },
+    {
+      "Lievelingskleur": "008000"
+    },
+    {
+      "Lievelingskleur": "66"
+    },
+    {
+      "Lievelingskleur": "#964B00"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#45eded"
+    },
+    {
+      "Lievelingskleur": "#35682d"
+    },
+    {
+      "Lievelingskleur": "#ffc0cb"
+    },
+    {
+      "Lievelingskleur": "#616567"
+    },
+    {
+      "Lievelingskleur": "GEEL"
+    },
+    {
+      "Lievelingskleur": "#7c23a8"
+    },
+    {
+      "Lievelingskleur": "CFEEFA"
+    },
+    {
+      "Lievelingskleur": "#0093FF"
+    },
+    {
+      "Lievelingskleur": "#167505"
+    },
+    {
+      "Lievelingskleur": "4EEA43"
+    },
+    {
+      "Lievelingskleur": "#000000"
+    },
+    {
+      "Lievelingskleur": "#40E0D0"
+    },
+    {
+      "Lievelingskleur": "#000"
+    }
+  ]
+
+colors.indexOf
